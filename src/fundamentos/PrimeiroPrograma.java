94cb1c5428f84264a68ffb8569d5ec023d8dfed5
Pedro Ulhoa <pedroulhoa@live.com>
2020-09-21 20:42:51 -0300

    feat: Primeiro Programa e Comentários

---
+++ src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,19 @@
+package fundamentos;
+
+/**
+ * Essa classe representa um exemplo básico da linguagem.
+ *
+ * @author Pedro
+ * @since 1.0
+ */
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+
+        // comentário de uma linha
+        System.out.println("Hello world!");
+        System.out.println("Hello world! #2");
+
+    }
+
+}
