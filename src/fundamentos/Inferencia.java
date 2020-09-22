8414fe9a19634b6d4e1322121565c85af793812d
Pedro Ulhoa <pedroulhoa@live.com>
2020-09-21 21:12:39 -0300

    feat: Inferencia de tipos

---
+++ src/fundamentos/Inferencia.java
@@ -0,0 +1,21 @@
+package fundamentos;
+
+public class Inferencia {
+
+    public static void main(String[] args) {
+
+        var a = 4.5;
+        System.out.println(a);
+
+        a = 12.57;
+        System.out.println(a);
+
+        var c = "Texto";
+        System.out.println(c);
+
+        c = "Outro texto";
+        System.out.println(c);
+
+    }
+
+}
