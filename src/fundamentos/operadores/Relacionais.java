9a8d5c780581d69ef8c7a4fd0852577672b50924
Pedro Ulhoa <pedroulhoa@live.com>
2020-12-29 20:42:11 -0300

    new: Operadores de Atribuicao

--- src/fundamentos/operadores/Relacionais.java
+++ src/fundamentos/operadores/Relacionais.java
@@ -2,7 +2,7 @@ package fundamentos.operadores;
 
 public class Relacionais {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         int a = 97;
         int b = 'a';
