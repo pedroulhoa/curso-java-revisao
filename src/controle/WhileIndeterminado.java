eab1f3d8cbbaeb82188b150a076d5a61474fc091
Pedro Ulhoa <pedroulhoa@live.com>
2021-01-08 16:28:59 -0300

    new: while indeterminado

---
+++ src/controle/WhileIndeterminado.java
@@ -0,0 +1,21 @@
+package controle;
+
+import java.util.Scanner;
+
+public class WhileIndeterminado {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String valor = "";
+
+        while (!valor.equalsIgnoreCase("sair")) {
+            System.out.println("Você diz: ");
+            valor = sc.nextLine();
+        }
+
+        sc.close();
+    }
+
+}
