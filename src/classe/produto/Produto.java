c9391b95d2fabba1059e95a9f1951dd91179e9b8
Pedro Ulhoa <pedroulhoa@live.com>
2021-01-26 22:15:43 -0300

    new: construtor produto

--- src/classe/produto/Produto.java
+++ src/classe/produto/Produto.java
@@ -6,6 +6,19 @@ public class Produto {
     double preco;
     double desconto;
 
+    public Produto() {
+    }
+
+    public Produto(String nome) {
+        this.nome = nome;
+    }
+
+    public Produto(String nome, double preco, double desconto) {
+        this.nome = nome;
+        this.preco = preco;
+        this.desconto = desconto;
+    }
+
     double precoComDesconto() {
         return preco * (1 - desconto);
     }
