5a88c861e7175fa2926fd66b81847223056743a8
Pedro Ulhoa <pedroulhoa@live.com>
2021-01-26 22:30:50 -0300

    new: construtor data

--- src/classe/data/Data.java
+++ src/classe/data/Data.java
@@ -6,6 +6,18 @@ public class Data {
     Integer mes;
     Integer ano;
 
+    public Data() {
+        dia = 1;
+        mes = 1;
+        ano = 1970;
+    }
+
+    public Data(Integer dia, Integer mes, Integer ano) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+
     String obterDataFormatada() {
         return String.format("%d/%d/%d", dia, mes, ano);
     }
--- src/classe/data/DataTeste.java
+++ src/classe/data/DataTeste.java
@@ -5,14 +5,7 @@ public class DataTeste {
     public static void main(String[] args) {
 
         Data d1 = new Data();
-        d1.dia = 05;
-        d1.mes = 10;
-        d1.ano = 1995;
-
-        Data d2 = new Data();
-        d2.dia = 13;
-        d2.mes = 9;
-        d2.ano = 1997;
+        Data d2 = new Data(05, 10, 1995);
 
         System.out.printf(d1.obterDataFormatada());
         System.out.println();
