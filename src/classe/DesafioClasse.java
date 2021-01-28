2e95c323ccb956d5d9bad34838a2bacc9b0f6aba
Pedro Ulhoa <pedroulhoa@live.com>
2021-01-27 22:09:24 -0300

    new: desafio static/instancia

---
+++ src/classe/DesafioClasse.java
@@ -0,0 +1,17 @@
+package classe;
+
+public class DesafioClasse {
+
+    int a = 3;
+    static int b = 4;
+
+    public static void main(String[] args) {
+
+        DesafioClasse desafioClasse = new DesafioClasse();
+        System.out.println(desafioClasse.a);
+
+        System.out.println(b);
+
+    }
+
+}
