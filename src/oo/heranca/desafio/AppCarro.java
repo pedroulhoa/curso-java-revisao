117fec7bfa2fc87bddba7d0b5eb0b8064386be57
Pedro Ulhoa <pedroulhoa@live.com>
2021-02-15 21:43:25 -0300

    new: desafio heranca

---
+++ src/oo/heranca/desafio/AppCarro.java
@@ -0,0 +1,34 @@
+package oo.heranca.desafio;
+
+public class AppCarro {
+
+    public static void main(String[] args) {
+
+        Civic civic = new Civic();
+        Ferrari ferrari = new Ferrari();
+
+        System.out.println("Velocidade Civic: " + civic.velocidadeAtual);
+        System.out.println("Velocidade Ferrari: " + ferrari.velocidadeAtual);
+
+        civic.acelerar();
+        civic.acelerar();
+        civic.acelerar();
+
+        ferrari.acelerar();
+        ferrari.acelerar();
+        ferrari.acelerar();
+
+        civic.frear();
+        civic.frear();
+        civic.frear();
+        civic.frear();
+        civic.frear();
+        ferrari.frear();
+
+        System.out.println("Velocidade Civic: " + civic.velocidadeAtual);
+        System.out.println("Velocidade Ferrari: " + ferrari.velocidadeAtual);
+
+
+    }
+
+}
