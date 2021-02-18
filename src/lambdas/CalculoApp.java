5b59dd759f7f679a396c2c2d04cdec5774fb5517
Pedro Ulhoa <pedroulhoa@live.com>
2021-02-17 21:10:21 -0300

    new: funcao lambda exemplo 1

---
+++ src/lambdas/CalculoApp.java
@@ -0,0 +1,17 @@
+package lambdas;
+
+public class CalculoApp {
+
+    public static void main(String[] args) {
+
+        Calculo calc = (x, y) -> {
+            return x + y;
+        };
+        System.out.println(calc.executar(2, 3));
+
+        calc = (x, y) -> x * y;
+        System.out.println(calc.executar(2, 3));
+
+    }
+
+}
