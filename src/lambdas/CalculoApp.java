45f0faefb209c5f1d25b703262e7d2ac610fb359
Pedro Ulhoa <pedroulhoa@live.com>
2021-02-17 21:36:06 -0300

    new: funcao lambda/Interface funcionais

--- src/lambdas/CalculoApp.java
+++ src/lambdas/CalculoApp.java
@@ -1,9 +1,12 @@
 package lambdas;
 
+import java.util.function.BinaryOperator;
+
 public class CalculoApp {
 
     public static void main(String[] args) {
 
+        // Utilizando interface funcional criada na mão:
         Calculo calc = (x, y) -> {
             return x + y;
         };
@@ -11,6 +14,15 @@ public class CalculoApp {
 
         calc = (x, y) -> x * y;
         System.out.println(calc.executar(2, 3));
+
+        // ----------------------------------------------------
+
+        // Utilizando interface funcional já existente na API do Java:
+        BinaryOperator<Double> calcExeploBinaryOperator = (x, y) -> {return x + y;};
+        System.out.println(calcExeploBinaryOperator.apply(1.0, 3.0));
+
+        calcExeploBinaryOperator = (x, y) -> x + y;
+        System.out.println(calcExeploBinaryOperator.apply(2.0, 3.0));
 
     }
 
