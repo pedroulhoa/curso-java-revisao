403485ca61d92c4571405a0851e9c47f85493a60
Pedro Ulhoa <pedroulhoa@live.com>
2021-03-04 22:33:17 -0300

    new: print streams

---
+++ src/streams/PrintListUsingStream.java
@@ -0,0 +1,32 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class PrintListUsingStream {
+
+    public static void main(String[] args) {
+
+        List<String> aprovados = Arrays.asList("Lu", "Gui", "Luca", "Ana");
+
+        for (String nome : aprovados) {
+            System.out.println(nome);
+        }
+
+        System.out.println("\n");
+
+        Iterator<String> iterator = aprovados.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+        System.out.println("\n");
+
+        Stream<String> stream = aprovados.stream();
+        stream.forEach(System.out::println);
+
+    }
+
+}
