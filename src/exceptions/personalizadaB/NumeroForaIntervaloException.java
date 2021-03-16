e830b4bcbbe891cbddb02240ae27967a25a19fbe
Pedro Ulhoa <pedroulhoa@live.com>
2021-03-16 11:45:25 -0300

    new: Exemplo exceções personalizadas com exceções checadas

---
+++ src/exceptions/personalizadaB/NumeroForaIntervaloException.java
@@ -0,0 +1,15 @@
+package exceptions.personalizadaB;
+
+public class NumeroForaIntervaloException extends Exception {
+
+    private String nomeDoAtributo;
+
+    public NumeroForaIntervaloException(String nomeDoAtributo) {
+        this.nomeDoAtributo = nomeDoAtributo;
+    }
+
+    @Override
+    public String getMessage() {
+        return String.format("O atributo '%s' está fora do intervalo.", nomeDoAtributo);
+    }
+}
---
+++ src/exceptions/personalizadaB/Validar.java
@@ -0,0 +1,17 @@
+package exceptions.personalizadaB;
+
+import exceptions.Aluno;
+
+public class Validar {
+
+    private Validar() {
+    }
+
+    public static void aluno(Aluno aluno) throws StringVaziaException, NumeroForaIntervaloException {
+
+        if (aluno == null) throw new IllegalArgumentException("O aluno está nulo!");
+        if (aluno.name == null || aluno.name.trim().isEmpty()) throw new StringVaziaException("nome");
+        if (aluno.nota < 0 || aluno.nota > 10) throw new NumeroForaIntervaloException("nota");
+
+    }
+}
