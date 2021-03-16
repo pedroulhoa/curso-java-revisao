85c6c7b9d70eb5c87d7ee6d89ac3fd178e997f8e
Pedro Ulhoa <pedroulhoa@live.com>
2021-03-16 08:17:47 -0300

    new: Exceptions/try...catch

---
+++ src/exceptions/Aluno.java
@@ -0,0 +1,35 @@
+package exceptions;
+
+import java.util.Objects;
+
+public class Aluno {
+
+    final String name;
+    final double nota;
+
+    public Aluno(String name, double nota) {
+        this.name = name;
+        this.nota = nota;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Aluno aluno = (Aluno) o;
+        return Double.compare(aluno.nota, nota) == 0 && Objects.equals(name, aluno.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, nota);
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" +
+                "name='" + name + '\'' +
+                ", nota=" + nota +
+                '}';
+    }
+}
